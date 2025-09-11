ddf7422a1f41a95b7c2c24cb2ef2c5f5f4ec306c
Divyanshu Ojha <divyanshuojha9454@gmail.com>
2025-09-12 00:16:29 +0530

    first commit

---
+++ addition.py
@@ -0,0 +1,7 @@
+#Addition
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: ")) 
+num3 = int(input("Enter the 3rd number: "))  
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
