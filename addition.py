ab6030bd244d95a54d320324409e5a295796be05
Divyanshu Ojha <divyanshuojha9454@gmail.com>
2025-09-18 08:18:58 +0530

    first commit

--- addition.py
+++ addition.py
@@ -3,5 +3,5 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: ")) 
 num3 = int(input("Enter the 3rd number: "))  
-sum_result = num1 + num2
+sum_result = num1 + num2 + num2
 print(f"sum: {num1} + {num2} = {sum_result}")
